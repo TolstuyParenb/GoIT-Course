e6b2a8f38e5c8b9553c6f090d145510bc7bafb64
Aleksey <tolstuy.paren@gmail.com>
2021-03-29 17:01:59 +0300

    Set modals

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,6 @@
-const technologiesSelect = document.querySelector('#calculator-form-technologies')
+const technologiesSelect = document.querySelector('#calculator-form-technologies');
+
+
 const technologiesMultiSelect = new Choices(technologiesSelect, {
     allowSearch: false,
     silent: false,
@@ -25,3 +27,78 @@ const technologiesMultiSelect = new Choices(technologiesSelect, {
         input: "choices__input",
     },
 });
+
+calculateSum();
+
+const calculatorform = document.querySelector('.calculator-form');
+
+calculatorform.addEventListener('submit', function(event) {
+    event.preventDefault();
+    calculateSum();
+});
+
+function calculateSum() {
+    // Selectors
+    const websiteTipeSelect = document.querySelector('#calculator-form-website-tipe');
+    const websiteCart = document.querySelector('#calculator-form-input-cart input:checked');
+    const websiteEmail = document.querySelector('#calculator-form-input-email input:checked');
+
+
+
+    // Values
+    const websiteTipeValue = extractPriceFromValue(websiteTipeSelect.value);
+    const technologiesValue = getTechnologiesSum(technologiesMultiSelect.getValue());
+    const websiteCartValue = convertCartOptionToPrice(websiteCart.value);
+    const websiteEmailValue = convertEmailOptionToPrice(websiteEmail.value);
+
+
+    const totalSum = websiteTipeValue + technologiesValue + websiteCartValue + websiteEmailValue;
+
+    renderSum(totalSum);
+}
+
+function renderSum(sum) {
+    const costElement = document.querySelector('.calculator-form-total');
+    
+    costElement.textContent = 'Calculating ...';
+    setTimeout(function () {
+        costElement.textContent = sum + '$'
+    }, 1500);
+}
+
+function convertCartOptionToPrice(option) {
+    if (option === 'Yes') {
+        return 350;
+    }
+    return 0;
+}
+
+
+
+function convertEmailOptionToPrice(option) {
+    if (option === 'Yes') {
+        return 500;
+    }
+    return 0;
+}
+
+
+function getTechnologiesSum(technologiesArr) {
+    let totalSum = 0;
+
+    technologiesArr.forEach(function(tech) {
+        totalSum = totalSum + extractPriceFromValue(tech.value)
+    })
+    return totalSum;
+}
+
+
+function extractPriceFromValue(str) {
+    const price = str.match(/:\d+/);
+
+    if (price) {
+        return Number(price[0].slice(1)) || 0;
+    }
+
+    return 0;
+}
